4ebc16cda08bd51631661fccf0a42bc76e2ae70f
Leonardo Galindo <61255738+GSLeonardo@users.noreply.github.com>
2021-09-06 11:16:41 -0400

    Styling Table Cell

---
+++ DonationsTracker/ViewController/EventsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EventsViewController.swift
+//  DonationsTracker
+//
+//  Created by user194269 on 9/6/21.
+//  Copyright © 2021 Team5. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UITableViewController {
+    
+}
