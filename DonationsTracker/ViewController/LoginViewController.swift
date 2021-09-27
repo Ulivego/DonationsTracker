511b807dc97722cd60f9695418cbb4e055904c8d
RobTecGda <78834009+RobTecGda@users.noreply.github.com>
2021-09-27 16:27:54 -0400

    Login Implementation

--- DonationsTracker/ViewController/LoginViewController.swift
+++ DonationsTracker/ViewController/LoginViewController.swift
@@ -15,8 +15,19 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var correoTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
+    let loginToList = "LoginToMain"
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        correoTF.delegate = self
+        passwordTF.delegate = self
         
         correoTF.layer.borderWidth = 2
         correoTF.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
@@ -32,8 +43,62 @@ class LoginViewController: UIViewController {
         
         view.addGestureRecognizer(tap)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        handle = Auth.auth().addStateDidChangeListener{ _, user in
+            if user == nil{
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+            else {
+                self.performSegue(withIdentifier: self.loginToList, sender: nil)
+                self.correoTF.text = nil
+                self.passwordTF.text = nil
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        guard let handle = handle else{return }
+        
+        Auth.auth().removeStateDidChangeListener(handle)
+    }
+    
+    @IBAction func loginDidTouch(_ sender: AnyObject){
+        guard
+            let email = correoTF.text,
+            let password = passwordTF.text,
+            !email.isEmpty,
+            !password.isEmpty
+        else{
+            return
+        }
+        
+        // Start login
+        Auth.auth().signIn(withEmail: email, password: password){
+            user, error in
+            if let error = error, user == nil{
+                let alert = UIAlertController(
+                    title: "Sign in failed",
+                    message: error.localizedDescription,
+                    preferredStyle: .alert
+                )
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
 
-    @IBAction func loginBtn(_ sender: Any) {
+    /*@IBAction func loginBtn(_ sender: Any) {
         
         if  let email = correoTF.text,
             let password = passwordTF.text {
@@ -57,7 +122,7 @@ class LoginViewController: UIViewController {
                 
             }
         }
-    }
+    }*/
     
     @IBAction func registerBtn(_ sender: Any) {
     }
@@ -67,3 +132,14 @@ class LoginViewController: UIViewController {
     }
 }
 
+extension LoginViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == correoTF {
+            passwordTF.becomeFirstResponder()
+        }
+        if textField == passwordTF{
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+}
