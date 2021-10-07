ee53e5d326c80678d4c5d08800334d50795b5302
Ulises <ulivego@hotmail.com>
2021-10-07 11:42:32 -0500

    Trying to delete from Firebase

--- DonationsTracker/ViewController/ProductCollectionViewController.swift
+++ DonationsTracker/ViewController/ProductCollectionViewController.swift
@@ -116,6 +116,11 @@ class GoalsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             tableView.beginUpdates()
+            
+            let toDelete = self.ref.child("Goals").child(products[indexPath.row])
+            ref.removeValue { error, _ in
+                print(error.localizedDescription)
+            }
                 
             products.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
