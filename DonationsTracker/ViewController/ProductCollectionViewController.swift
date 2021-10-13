d6b6096b502179f6421c12d57145f67e80aedb13
Ulises <ulivego@hotmail.com>
2021-10-13 13:00:12 -0400

    Product Sort, Delete Product upon completion and Progress bar color
    Added these things:Product Sort, Delete Product upon completion and Progress bar color

--- DonationsTracker/ViewController/ProductCollectionViewController.swift
+++ DonationsTracker/ViewController/ProductCollectionViewController.swift
@@ -38,6 +38,7 @@ class GoalsViewController: UITableViewController {
                 }
             }
             self.products = newGoals
+            self.products.sort(by: self.sortProgress)
             self.tableView.reloadData()
 
         }
@@ -64,18 +65,31 @@ class GoalsViewController: UITableViewController {
     
         let cell = tableView.dequeueReusableCell(withIdentifier: "Item Cell", for: indexPath) as! ProductCell
         
-        self.Goals.sortInPlace({$0.progress/$0.goal < $1.progress/$1.goal})
         
         let goal = products[indexPath.row]
         
         cell.productLabel.text = goal.product //Ver como dar valor
         cell.donadosLabel.text = String(goal.progress) + " kg"
         cell.objetivoLabel.text = String(goal.goal) + " kg"
+        if(indexPath.row % 2 == 0){
+            cell.progressBar.progressTintColor = UIColor(red: 252/255, green: 161/255, blue: 47/255, alpha: 1)
+        } else{
+            cell.progressBar.progressTintColor = UIColor(red: 247/255, green: 0/255, blue: 56/255, alpha: 1)
+        }
         cell.progressBar.setProgress(Float(goal.progress) / Float(goal.goal) , animated: true)
         if cell.progressBar.bounds.height == 0{
-            cell.progressBar.transform = cell.progressBar.transform.scaledBy(x: 1, y: 4)        }
+            cell.progressBar.transform = cell.progressBar.transform.scaledBy(x: 1, y: 4)
+        }
         print(cell.progressBar.bounds.size)
-    
+        
+        if goal.progress >= goal.goal {
+            let toDelete = self.ref.child("Goals").child(products[indexPath.row].key)
+            toDelete.removeValue { error, _ in
+                print(error?.localizedDescription as Any)
+            }
+            products.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
         return cell
     }
     
@@ -106,6 +120,10 @@ class GoalsViewController: UITableViewController {
                 ]
                 if(goalText != "" || productText != ""){
                     self.ref.child("Goals").childByAutoId().setValue(object)
+                } else{
+                    let notCreated = UIAlertController(title: "Goal not created", message: "Product was empty so the goal wasn't created", preferredStyle: .alert)
+                    notCreated.addAction(UIAlertAction(title: "Ok", style: .default))
+                    self.present(notCreated, animated: true)
                 }
             }
                
@@ -135,12 +153,10 @@ class GoalsViewController: UITableViewController {
         }
     }
     
-    //Testing function to sort the goals //Update: didn't work
-    /*
-    func sortItemsByProgress(_ products: [Goal]) -> [Goal] {
-        products.sorted {itemA, itemB in
-            itemA.progress / itemA.goal < itemB.progress / itemB.goal
-        }
+    func sortProgress(this: Goal, that: Goal) -> Bool {
+        let value1 = Float(this.progress) / Float(this.goal)
+        let value2 = Float(that.progress) / Float(that.goal)
+        return value1 < value2
     }
-    */
+    
 }
